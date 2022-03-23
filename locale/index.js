1c242b1a1b3144daac9328666a5c18be41349ab6
ying ma <maying0104@gmail.com>
2022-03-23 18:30:00 +0800

    first commit

---
+++ locale/index.js
@@ -0,0 +1,90 @@
+const messages = {
+    en: {
+        // 导航
+        aboutMe: "About Me",
+        download: "Download",
+        documents: "Documents",
+        help: "Help",
+
+        // 按钮文案
+        disconnect: "disconnect",
+        connect: "connect",
+
+        // 无线串口通信
+        wirelessSerialCommunication: "wireless serial communication",
+
+        // 波特率
+        baudRate: "baud rate",
+        baudRateHolder: "please select baud rate",
+
+        // 环境
+        environmentalStateDetection: "environmental state detection",
+        longitude: "longitude",
+        latitude: "latitude",
+        localTime: "local time",
+        localTemperature: "local temperature",
+        localHumidity: "local humidity",
+        localWindSpeed: "local wind speed",
+
+        // 支架状态信息
+        supportState: "support state message",
+        standNumber: "stand number",
+        componentAngle: "component angle",
+        motorBattery: "motor battery",
+        motorVoltage: "motor voltage",
+        motorCurrent: "motor vurrent",
+        stentsAbnormalAlarm: "stents abnormal alarm",
+
+        // 支架控制面板
+        supportControlPanel: "support control panel",
+        controlMode: "control mode",
+        automaticMode: "automatic mode",
+        masterReset: "master reset",
+        angleControl: "angle control",
+        turnToSpecifiedAngle: "turn to specified angle",
+    },
+    zh: {
+        // 导航
+        aboutMe: "关于我",
+        download: "下载",
+        documents: "文档",
+        help: "帮助",
+
+        // 按钮文案
+        disconnect: "断开",
+        connect: "连接",
+
+        // 无线串口通信
+        wirelessSerialCommunication: "无线串口通信",
+
+        // 波特率
+        baudRate: "波特率",
+        baudRateHolder: "请选择波特率",
+
+        // 环境
+        environmentalStateDetection: "环境状态检测",
+        longitude: "经度",
+        latitude: "纬度",
+        localTime: "当地时间",
+        localTemperature: "当地温度",
+        localHumidity: "当地湿度",
+        localWindSpeed: "当地风速",
+
+        // 支架状态信息
+        supportState: "支架状态信息",
+        standNumber: "支架编号",
+        componentAngle: "组件倾角",
+        motorBattery: "电池电量",
+        motorVoltage: "电机电压",
+        motorCurrent: "电机电流",
+        stentsAbnormalAlarm: "支架异常报警",
+
+        // 支架控制面板
+        supportControlPanel: "支架控制面板",
+        controlMode: "控制模式",
+        automaticMode: "自动模式",
+        masterReset: "一键复位",
+        angleControl: "角度控制",
+        turnToSpecifiedAngle: "转到指定角度",
+    },
+};
